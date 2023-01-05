931d8ff30db4481b7d13e13a46ef898136a00b06
usama-365 <usama-365@protonmail.com>
2023-01-05 18:42:15 +0500

    Made the category dynamic on homepage

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-const App = () => (
-    <div className="categories">
-        <div className="category">
-            {/* Image */}
-            <div className="category__body">
-                <h2>Hats</h2>
-                <p>Shop Now</p>
-            </div>
+const App = function () {
+    const categories = [
+        {
+            id: 1,
+            title: "Hats"
+        },
+        {
+            id: 2,
+            title: "Jackets"
+        },
+        {
+            id: 3,
+            title: "Sneakers"
+        },
+        {
+            id: 4,
+            title: "Womens"
+        },
+        {
+            id: 5,
+            title: "Mens"
+        },
+    ];
+    return (
+        <div className="categories">
+            {categories.map(category => (
+                <div className="category">
+                    <div className="category__background" />
+                    <div className="category__body">
+                        <h2>{category.title}</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            ))}
         </div>
-        <div className="category">
-            {/* Image */}
-            <div className="category__body">
-                <h2>Jackets</h2>
-                <p>Shop Now</p>
-            </div>
-        </div>
-        <div className="category">
-            {/* Image */}
-            <div className="category__body">
-                <h2>Sneakers</h2>
-                <p>Shop Now</p>
-            </div>
-        </div>
-        <div className="category">
-            {/* Image */}
-            <div className="category__body">
-                <h2>Women</h2>
-                <p>Shop Now</p>
-            </div>
-        </div>
-        <div className="category">
-            {/* Image */}
-            <div className="category__body">
-                <h2>Men</h2>
-                <p>Shop Now</p>
-            </div>
-        </div>
-    </div>
-);
+    );
+}
 
 
 export default App;
