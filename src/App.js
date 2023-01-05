af9df11660fa95f51de8621a8e33dfb5560ce214
usama-365 <usama-365@protonmail.com>
2023-01-05 18:31:52 +0500

    Added basic markup structure and globally resetted styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
-
 const App = () => (
-    <div>Hello World</div>
+    <div className="categories">
+        <div className="category">
+            {/* Image */}
+            <div className="category__body">
+                <h2>Hats</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div className="category">
+            {/* Image */}
+            <div className="category__body">
+                <h2>Jackets</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div className="category">
+            {/* Image */}
+            <div className="category__body">
+                <h2>Sneakers</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div className="category">
+            {/* Image */}
+            <div className="category__body">
+                <h2>Women</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div className="category">
+            {/* Image */}
+            <div className="category__body">
+                <h2>Men</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    </div>
 );
 
 
