baf37b28144bc5348ff660aad62f8d1db65e6978
usama-365 <usama-365@protonmail.com>
2023-01-06 19:25:45 +0500

    Styled the navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,17 @@
 import {Link, Outlet} from "react-router-dom";
+import {ReactComponent as CrownLogo} from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 export const Navigation = function () {
     return (
         <>
             <nav className="navigation">
-                <Link className="navigation__logo-container" to='/'>
-                    <div className="navigation__logo">Logo</div>
+                <Link className="navigation__logo-container" to="/">
+                    <CrownLogo className="navigation__logo"/>
                 </Link>
                 <ul className="navigation__list">
                     <li className="navigation__item">
-                        <Link className="navigation__link" to='/shop'>Shop</Link>
+                        <Link className="navigation__link" to="/shop">Shop</Link>
                     </li>
                 </ul>
             </nav>
