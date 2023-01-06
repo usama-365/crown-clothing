7218e70c91a3c3e0e9ebcc3efffeead2f661a374
usama-365 <usama-365@protonmail.com>
2023-01-06 18:06:53 +0500

    Added ReactRouter library and home route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import {Categories} from "./components/categories/categories.component";
+import {Home} from "./routes/home/home.component";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    }
+]);
 
 const App = function () {
-    const categories = [
-        {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ];
     return (
-        <Categories categories={categories}/>
+        <RouterProvider router={router}/>
     );
 }
 
