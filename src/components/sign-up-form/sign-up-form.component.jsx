9d1958ee7d925e6d46fec7cb0484c5c5d3671e61
usama-365 <usama-365@protonmail.com>
2023-01-07 19:43:58 +0500

    Created styled form input component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {getOrCreateUserDocument, signUpWithEmailAndPassword} from "../../services/firebase/firebase.service";
+import {FormInput} from "../form-input/form-input.component";
 
 const defaultFormFields = {
     displayName: "",
@@ -33,29 +34,56 @@ export const SignUpForm = function () {
             resetFormFields();
         } catch (error) {
             switch (error.code) {
-                case 'auth/email-already-in-use':
-                    alert('Cannot create user, email already exists');
+                case "auth/email-already-in-use":
+                    alert("Cannot create user, email already exists");
                     break;
                 default:
                     alert(`User creation encountered an error: ${error}`);
                     break;
             }
         }
-
     }
 
     return (
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input name="displayName" type="text" onChange={handleChange} value={displayName} required/>
-                <label>Email</label>
-                <input name="email" type="text" onChange={handleChange} value={email} required/>
-                <label>Password</label>
-                <input name="password" type="password" onChange={handleChange} value={password} required/>
-                <label>Confirm Password</label>
-                <input name="confirmPassword" type="password" onChange={handleChange} value={confirmPassword} required/>
+                <FormInput
+                    label="Display Name"
+                    inputAttributes={{
+                        onChange: handleChange,
+                        value: displayName,
+                        name: "displayName",
+                        type: "text"
+                    }}
+                />
+                <FormInput
+                    label="Email"
+                    inputAttributes={{
+                        onChange: handleChange,
+                        value: email,
+                        name: "email",
+                        type: "text"
+                    }}
+                />
+                <FormInput
+                    label="Password"
+                    inputAttributes={{
+                        onChange: handleChange,
+                        value: password,
+                        name: "password",
+                        type: "password"
+                    }}
+                />
+                <FormInput
+                    label="Confirm Password"
+                    inputAttributes={{
+                        onChange: handleChange,
+                        value: confirmPassword,
+                        name: "confirmPassword",
+                        type: "password"
+                    }}
+                />
                 <button type="submit">Sign Up</button>
             </form>
         </div>
