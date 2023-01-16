256d974228df6547fbfa4750e8ae030a53d15bd9
usama-365 <usama-365@protonmail.com>
2023-01-16 16:16:52 +0500

    User context now uses reducer instead of state

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useEffect, useState} from "react";
+import {createContext, useEffect, useReducer} from "react";
 import {onAuthStateChangedListener} from "../services/firebase/firebase.service";
 
 export const UserContext = createContext({
@@ -6,8 +6,34 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+};
+
+const userReducer = function (state, action) {
+    const {type, payload} = action;
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            };
+        default:
+            throw new Error(`Unhandled action type provided to userReducer: ${type}`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserContextProvider = function ({children}) {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    const {currentUser} = state;
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+    }
+
     const value = {
         currentUser,
         setCurrentUser
