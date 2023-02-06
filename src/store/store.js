095214c4918be3f96927313aa577dcae70b80a0a
usama-365 <usama-365@protonmail.com>
2023-02-06 15:00:18 +0500

    Started integrating redux into the app

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,8 @@
-import {compose, createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+import {configureStore} from "@reduxjs/toolkit";
+import {rootReducer} from "./root-reducer";
+import logger from "redux-logger";
 
-// root-reducer, combination of all reducers
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
+});
