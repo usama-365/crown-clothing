01e6da8bddbd8f91cf10c05d647a98fc01bfce3a
usama-365 <usama-365@protonmail.com>
2023-02-08 15:08:00 +0500

    Added memoization/caching using createSelector

--- src/store/categories/categories.selectors.js
+++ src/store/categories/categories.selectors.js
@@ -1,5 +1,17 @@
-export const selectCategoriesMap = state => state.categories.categories.reduce((acc, category) => {
-    const {title, items} = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {});
+import {createSelector} from "@reduxjs/toolkit";
+
+const selectCategoryReducer = state => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    categoriesSlice => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    categories => categories.reduce((acc, category) => {
+        const {title, items} = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+)
