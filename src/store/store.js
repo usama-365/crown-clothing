31566298e2df97644446870e7378f257caabaecf
usama-365 <usama-365@protonmail.com>
2023-02-08 19:52:18 +0500

    Minor refactoring

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,33 @@
-import {configureStore} from "@reduxjs/toolkit";import logger from "redux-logger";
-import storage from 'redux-persist/lib/storage';
+import {configureStore} from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import storage from "redux-persist/lib/storage";
 import {persistReducer, persistStore} from "redux-persist";
 import {rootReducer} from "./root-reducer";
 
 const persistConfig = {
-    key: 'root',
+    key: "root",
     storage,
-    blacklist: ['user'],
+    blacklist: ["user"],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware({
-        serializableCheck: false,
-        immutableCheck: false,
-        thunk: false
-    }).concat(logger)
+    middleware: getDefaultMiddleware => (
+        process.env.NODE_ENV === "development" ?
+            getDefaultMiddleware({
+                serializableCheck: false,
+                immutableCheck: false,
+                thunk: false
+            }).concat(logger)
+            :
+            getDefaultMiddleware({
+                serializableCheck: false,
+                immutableCheck: false,
+                thunk: false
+            })
+    )
 });
 
 export const persistor = persistStore(store);
