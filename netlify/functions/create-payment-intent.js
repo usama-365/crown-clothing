5a59d57ed8786d4ddd88c18564d98f94c56835b8
usama-365 <usama-365@protonmail.com>
2023-08-12 20:56:00 +0500

    Added netlify serverless functions to process payment at backend

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+const calculateAmount = items => items.reduce(
+	(totalAmount, {
+		quantity,
+		price,
+	}) => totalAmount + (quantity * price * 100),
+	0,
+);
+
+exports.handler = async function (event) {
+	const { items } = JSON.parse(event.body);
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: calculateAmount(items),
+		currency: "usd",
+		payment_method_types: [ "card" ],
+	});
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			clientSecret: paymentIntent.client_secret,
+		}),
+	};
+};
