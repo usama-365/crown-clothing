abfd9d1428838d17210d918937150c2e0196a9d4
Usama <usama-365@protonmail.com>
2023-09-21 13:09:02 +0500

    fix broken dependencies

--- src/components/category-items/category-items.component.tsx
+++ src/components/category-items/category-items.component.tsx
@@ -1,30 +1,31 @@
 import "./category-items.styles.scss";
-import {useParams} from "react-router-dom";
-import {ProductCard} from "../product-card/product-card.component";
-import {useSelector} from "react-redux";
-import {selectCategoriesIsLoading, selectCategoriesMap} from "../../store/categories/categories.selectors";
-import {Spinner} from "../spinner/spinner.component";
+import { useParams } from "react-router-dom";
+import { ProductCard } from "../product-card/product-card.component";
+import { useSelector } from "react-redux";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/categories.selectors";
+import { Spinner } from "../spinner/spinner.component";
 
 export const CategoryItems = function () {
-    const {category} = useParams();
-    const isLoading = useSelector(selectCategoriesIsLoading);
-    const categories = useSelector(selectCategoriesMap);
-    if (category)
-        return (
-            <>
-                <h2 className="category-items__title">{category}</h2>
-                {
-                    isLoading || !categories[category] ? (
-                        <Spinner/>
-                    ) : (
-                        <div className="category-items__items">
-                            {categories[category].map(product => (
-                                <ProductCard key={product.id} product={product}/>
-                            ))}
-                        </div>
-                    )
-                }
-
-            </>
-        );
+  const { category } = useParams();
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  const categories = useSelector(selectCategoriesMap);
+  if (category)
+    return (
+      <>
+        <h2 className="category-items__title">{category}</h2>
+        {isLoading || !categories[category] ? (
+          <Spinner />
+        ) : (
+          <div className="category-items__items">
+            {categories[category].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        )}
+      </>
+    );
+  else return <p>Something went wrong</p>;
 };
